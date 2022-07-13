9c7a6bca01e77fa9c8b9fae0413b4d298b3e76eb
jtv6 <verle014@umn.edu>
2022-07-12 21:19:22 -0500

    first commit

---
+++ index.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/hello/FUCK')
+def hello():
+    return 'Hello World'
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+   app.run()
---
+++ templates/jiggle.py
@@ -0,0 +1,9 @@
+import pyautogui
+
+for x in range(9999):
+    pyautogui.moveTo(100,150)
+    pyautogui.moveRel(0,10)
+
+    pyautogui.moveTo(100,150)
+    pyautogui.moveRel(0,10)
+    x+=1
